3d94718fa88e238e620630b124b5886451d4dfac
Luisa Chen <luisachen95@gmail.com>
2018-03-15 10:30:24 +0100

    :sparkles: Add user profile component

---
+++ src/components/UserProfileImage.vue
@@ -0,0 +1,21 @@
+<template>
+  <figure
+    class="image is-96x96">
+    <img
+      :src="src"
+      style="border-radius:50%">
+  </figure>
+</template>
+
+<script>
+  export default {
+    props: {
+      src: {
+        default () {
+          return 'https://api.adorable.io/avatars/96/qweqw.png'
+        },
+        type: String
+      }
+    }
+  }
+</script>
